18dc63928b72babc6dd415ee08f5d385287333e4
charlie <cc258@qq.com>
2026-01-22 17:02:28 +0800

    feat: 请求用户列表，成功

--- api/users.py
+++ api/users.py
@@ -11,8 +11,16 @@ from typing import List
 
 router = APIRouter(prefix="/users", tags=["users"])
 
+# 获取所有用户
+@router.get("", response_model=List[UserSchema])
+def get_user(db: Session = Depends(get_db)):
+    users = db.query(UserModel)
+    return users
+
+# 获取指定用户
 @router.get("/{user_id}", response_model=UserSchema)
 def get_user(user_id: int, db: Session = Depends(get_db)):
     user = db.query(UserModel).filter(UserModel.id == user_id).first()
     return user
+
 
