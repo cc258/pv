1e3ca7e8dcadb77d38df11e48818e49e5ff0ffe6
Charlie <cc258@qq.com>
2026-08-09 10:31:56 +0800

    feat: Arco Pro → 朋友影视-后台管理

--- admin/src/components/NavBar/index.tsx
+++ admin/src/components/NavBar/index.tsx
@@ -145,7 +145,7 @@ function Navbar({ show }: { show: boolean }) {
       <div className={styles.left}>
         <div className={styles.logo}>
           <Logo />
-          <div className={styles['logo-name']}>Arco Pro</div>
+          <div className={styles['logo-name']}>朋友影视-后台管理</div>
         </div>
       </div>
       <ul className={styles.right}>
